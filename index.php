8ef5e95b3c262ddd7ca5ac17759d62bc3b44cab9
asiviero <altsiviero@gmail.com>
2013-06-05 15:48:30 -0300

    Most code by Fernando Zanardo (fzmaster), implemmenting a 2-step form, CSS improvements needed

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <html>
 <head>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/mailgration.js"></script>
 <title>Assistente de Migração de E-mail</title>
 </head>
 <body>
 <h1>Assistente de Migração de E-mail</h1>
 <p>Lorem Ipsum Dolor Sit Amet</p>
-<form action="archivesetup.php" method="POST">
+<form action="archivesetup.php" method="POST" id="mailgration-form">
 <p class="origem">Origem:
 <br />
 Endereço do Servidor (sem Portas ou especificações de protocolos):
 <input type="text" size="60" name="src_server" /><br />
 Usuário de E-mail: <input type="text" size="60" name="src_username" /><br />
 Senha: <input type="password" size="20" name="src_password" /><br />
+</p>
+<input type="button" id="load-inbox" name="load-inbox" value="Carregar Inboxes" />
+<p class="select-inbox">
+<select name="inboxes" id="inboxes-select" multiple="multiple"></select>
 </p>
 <p class="destino">
 Destino:
---
+++ js/mailgration.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('#load-inbox').click(function() {
+    $.ajax(
+      {
+        url: 'getinbox.php',
+        type: 'POST',
+        data: $('#mailgration-form').serialize(),
+        success: function(data) {
+          if(data.length) {
+            $('#inboxes-select').append(data);
+            $('p.select-inbox').show();
+          } 
+        }        
+      }       
+    );
+    
+  });
+  $('#mailgration-form').
+});
